1f71245eca0cfdc6f5f87dd316adb2c76c8a5aaa
Paul Virally <pvirally@gmail.com>
2022-08-22 21:52:23 -0400

    Added a little bit of documentation

---
+++ README.md
@@ -0,0 +1,10 @@
+# ψdb
+ψdb is a sci(ψ)entific database management system designed to help you remember which datasets correspond to which experiments!
+
+# TODO
+* [ ] GUI
+   - [ ] Visualize the entries in the database
+   - [ ] Add data to the database through the GUI
+* [ ] Data entegrity
+    - [ ] Make a backup of the database before writing the new database
+    - [ ] Maybe use SQL-style commits/transactions?
--- src/main.rs
+++ src/main.rs
@@ -13,47 +13,63 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
+    /// Initialize the database
     Init(Init),
+    /// Add a number of data files to the database
     AddData(AddData),
+    /// Add a transform (a script that modifies data) to the database
     AddTransform(AddTransform),
+    /// Connect multiple transforms/data sets together
     Connect(Connect),
+    /// Apply a tranform to a dataset, creating a new dataset
     Apply(Apply),
+    /// Chain multiple datasets together, creating a new dataset
     Chain(Chain),
+    /// Link multiple transforms together, creating a new transform
     Link(Link)
 }
 
 #[derive(Args)]
 struct Init {
+    /// Path to the database folder, defaults to $HOME/.psidb/
     #[clap(value_parser)]
     db_path: Option<String>,
 }
 
 #[derive(Args)]
 struct AddData {
+    /// Path to the database folder, defaults to $HOME/.psidb/
     #[clap(long = "db")]
     db_path: Option<String>,
     
+    /// The metadata associated with this dataset
     #[clap(long = "md")]
     meta_data: Option<String>,
 
+    /// The paths to the data that make up a dataset
     #[clap(value_parser)]
     data_paths: Vec<String>
 }
 
 #[derive(Args)]
 struct AddTransform {
+    /// Path to the database folder, defaults to $HOME/.psidb/
     #[clap(long = "db")]
     db_path: Option<String>,
     
+    /// The metadata associated with this dataset
     #[clap(long = "md")]
     meta_data: Option<String>,
 
+    /// The paths to the scripts that make up a transform
     #[clap(value_parser)]
     script_paths: Vec<String>,
 
+    /// The arguments passed to each script, one string per script
     #[clap(long = "args")]
     script_args: Option<String>,
 
+    /// The commits of the versions of the scripts if they are tracked by git, one string per script
     #[clap(long = "hashes")]
     script_git_hashes: Option<String>
 }
@@ -66,63 +82,80 @@ struct AddTransform {
         .args(&["in-data-ids", "out-data-ids", "in-transform-ids", "out-transform-ids"])
 ))]
 struct Connect {
+    /// Path to the database folder, defaults to $HOME/.psidb/
     #[clap(long = "db")]
     db_path: Option<String>,
 
+    /// The metadata associated with this dataset
     #[clap(long = "md")]
     meta_data: Option<String>,
 
+    /// The action of the connection, one of apply, chain, or link
     #[clap(arg_enum, short, long)]
     action: Action,
 
+    /// The ids of the datasets forming the input to the connection
     #[clap(long)]
     in_data_ids: Option<Vec<u64>>,
 
+    /// The ids of the datasets forming the output of the connection
     #[clap(long)]
     out_data_ids: Option<Vec<u64>>,
 
+    /// The ids of the transforms forming the input to the connection
     #[clap(long)]
     in_transform_ids: Option<Vec<u64>>,
 
+    /// The ids of the transforms forming the output of the connection
     #[clap(long)]
     out_transform_ids: Option<Vec<u64>>,
 }
 
 #[derive(Args)]
 struct Apply {
+    /// Path to the database folder, defaults to $HOME/.psidb/
     #[clap(long = "db")]
     db_path: Option<String>,
 
+    /// The metadata associated with this dataset
     #[clap(long = "md")]
     meta_data: Option<String>,
 
+    /// The id of the transform to apply
     #[clap(short, long)]
     transform_id: u64,
 
+    /// The datasets to apply the transform to
     #[clap(short, long)]
     data_ids: Vec<u64>
 }
 
 #[derive(Args)]
 struct Chain {
+    /// Path to the database folder, defaults to $HOME/.psidb/
     #[clap(long = "db")]
     db_path: Option<String>,
 
+    /// The metadata associated with this dataset
     #[clap(long = "md")]
     meta_data: Option<String>,
 
+    /// The ids of the transforms to chain together
     #[clap(short, long)]
     transform_ids: Vec<u64>
 }
 
 #[derive(Args)]
 struct Link {
+    /// Path to the database folder, defaults to $HOME/.psidb/
     #[clap(long = "db")]
     db_path: Option<String>,
 
+    /// The metadata associated with this dataset
     #[clap(long = "md")]
     meta_data: Option<String>,
 
+    /// The ids of the datasets to link togeth
     #[clap(short, long)]
     data_ids: Vec<u64>
 }
